06087f9b08f3e829092ffd5ad0a42f5f915c2d16
Keijiro Takahashi <keijiro@gmail.com>
2017-12-26 23:07:44 +0900

    i

---
+++ Assets/PSlicer/Effector.cs
@@ -0,0 +1,165 @@
+using UnityEngine;
+using UnityEngine.Timeline;
+using System.Collections.Generic;
+
+namespace PSlicer
+{
+    [ExecuteInEditMode]
+    [AddComponentMenu("Effects/PSlicer/Effector")]
+    class Effector : MonoBehaviour, ITimeControl
+    {
+        #region Editable attributes
+
+        [SerializeField] float _range = 1;
+        [SerializeField] float _offset = 0;
+
+        [Space]
+        [SerializeField] float _density = 20;
+        [SerializeField] float _speed = 5;
+
+        [SerializeField, ColorUsage(false, true, 0, 8, 0.125f, 3)]
+        Color _color;
+
+        [Space]
+        [SerializeField] Renderer[] _linkedRenderers;
+
+        #endregion
+
+        #region Utility properties for internal use
+
+        float LocalTime {
+            get {
+                if (_controlTime < 0)
+                    return Application.isPlaying ? Time.time : 0;
+                else
+                    return _controlTime;
+            }
+        }
+
+        #endregion
+
+        #region ITimeControl implementation
+
+        float _controlTime = -1;
+
+        public void OnControlTimeStart()
+        {
+        }
+
+        public void OnControlTimeStop()
+        {
+            _controlTime = -1;
+        }
+
+        public void SetTime(double time)
+        {
+            _controlTime = (float)time;
+        }
+
+        #endregion
+
+        #region MonoBehaviour implementation
+
+        MaterialPropertyBlock _sheet;
+
+        void LateUpdate()
+        {
+            if (_linkedRenderers == null || _linkedRenderers.Length == 0) return;
+
+            if (_sheet == null) _sheet = new MaterialPropertyBlock();
+
+            var mtx = transform.worldToLocalMatrix;
+            var time = LocalTime + 10;
+
+            foreach (var renderer in _linkedRenderers)
+            {
+                renderer.GetPropertyBlock(_sheet);
+
+                _sheet.SetFloat("_Density", _density);
+                _sheet.SetFloat("_Speed", _speed);
+
+                _sheet.SetFloat("_EffectorRange", _range);
+                _sheet.SetFloat("_EffectorOffset", _offset);
+                _sheet.SetMatrix("_EffectorMatrix", mtx);
+                _sheet.SetColor("_EffectorColor", _color);
+
+                _sheet.SetFloat("_LocalTime", time);
+
+                renderer.SetPropertyBlock(_sheet);
+            }
+        }
+
+        #endregion
+
+        #region Editor gizmo implementation
+
+        #if UNITY_EDITOR
+
+        Mesh _gridMesh;
+
+        void OnDestroy()
+        {
+            if (_gridMesh != null)
+            {
+                if (Application.isPlaying)
+                    Destroy(_gridMesh);
+                else
+                    DestroyImmediate(_gridMesh);
+            }
+        }
+
+        void OnDrawGizmos()
+        {
+            if (_gridMesh == null) InitGridMesh();
+
+            Gizmos.matrix = transform.localToWorldMatrix;
+
+            var p1 = Vector3.forward * _offset;
+            var p2 = Vector3.forward * (_offset + _range);
+
+            Gizmos.color = new Color(1, 1, 0, 0.5f);
+            Gizmos.DrawWireMesh(_gridMesh, p1);
+            Gizmos.DrawWireMesh(_gridMesh, p2);
+
+            Gizmos.color = new Color(1, 0, 0, 0.5f);
+            Gizmos.DrawWireCube((p1 + p2) / 2, new Vector3(0.02f, 0.02f, _range));
+        }
+
+        void InitGridMesh()
+        {
+            const float ext = 0.5f;
+            const int columns = 10;
+
+            var vertices = new List<Vector3>();
+            var indices = new List<int>();
+
+            for (var i = 0; i < columns + 1; i++)
+            {
+                var x = ext * (2.0f * i / columns - 1);
+
+                indices.Add(vertices.Count);
+                vertices.Add(new Vector3(x, -ext, 0));
+
+                indices.Add(vertices.Count);
+                vertices.Add(new Vector3(x, +ext, 0));
+
+                indices.Add(vertices.Count);
+                vertices.Add(new Vector3(-ext, x, 0));
+
+                indices.Add(vertices.Count);
+                vertices.Add(new Vector3(+ext, x, 0));
+            }
+
+            _gridMesh = new Mesh();
+            _gridMesh.hideFlags = HideFlags.DontSave;
+            _gridMesh.SetVertices(vertices);
+            _gridMesh.SetNormals(vertices);
+            _gridMesh.SetIndices(indices.ToArray(), MeshTopology.Lines, 0);
+            _gridMesh.UploadMeshData(true);
+        }
+
+        #endif
+
+        #endregion
+    }
+}
---
+++ Assets/PSlicer/PSlicer.shader
@@ -0,0 +1,103 @@
+﻿Shader "PSlicer"
+{
+    Properties
+    {
+        _MainTex("Albedo", 2D) = "white" {}
+        _Color("Color", Color) = (1, 1, 1, 1)
+        [Gamma] _Metallic("Metallic", Range(0, 1)) = 0
+        _Glossiness("Smoothness", Range(0, 1)) = 0.5
+        [HDR] _EmissionColor("Emission", Color) = (0, 0, 0)
+
+        [Header(Backface Attributes)]
+        _Color2("Color", Color) = (1, 1, 1, 1)
+        [Gamma] _Metallic2("Metallic", Range(0, 1)) = 0
+        _Glossiness2("Smoothness", Range(0, 1)) = 0.5
+
+        [HideInInspector] _EffectorColor("", Color) = (0, 0, 0)
+    }
+    SubShader
+    {
+        Tags { "RenderType"="TransparentCutout" "Queue"="AlphaTest" }
+
+        Cull Off
+
+        CGPROGRAM
+
+        #pragma surface surf Standard fullforwardshadows addshadow
+        #pragma target 3.0
+
+        struct Input
+        {
+            float2 uv_MainTex;
+            float3 worldPos;
+            float vface : VFACE;
+        };
+
+        sampler2D _MainTex;
+
+        half4 _Color;
+        half _Metallic;
+        half _Glossiness;
+        half3 _EmissionColor;
+
+        half4 _Color2;
+        half _Metallic2;
+        half _Glossiness2;
+
+        half _Density;
+        half _Speed;
+
+        float _EffectorRange;
+        float _EffectorOffset;
+        float4x4 _EffectorMatrix;
+        half3 _EffectorColor;
+
+        float _LocalTime;
+
+        // Hash function from H. Schechter & R. Bridson, goo.gl/RXiKaH
+        uint Hash(uint s)
+        {
+            s ^= 2747636419u;
+            s *= 2654435769u;
+            s ^= s >> 16;
+            s *= 2654435769u;
+            s ^= s >> 16;
+            s *= 2654435769u;
+            return s;
+        }
+
+        float Random(uint seed)
+        {
+            return float(Hash(seed)) / 4294967295.0; // 2^32-1
+        }
+
+        void surf(Input IN, inout SurfaceOutputStandard o)
+        {
+            float3 coord = mul(_EffectorMatrix, float4(IN.worldPos, 1));
+
+            float row = floor(coord.z * _Density);
+            uint seed = (uint)(row * 199 + 10000);
+            float speed = _Speed * (Random(seed) + 1) * 0.5;
+
+            float phi = atan2(coord.x, coord.y) * UNITY_INV_TWO_PI + 0.5;
+            phi = frac(phi + speed * _LocalTime);
+
+            float th = (row / _Density - _EffectorOffset) / _EffectorRange;
+
+            if (frac(phi) < th) discard;
+
+            float em = saturate(1 - (frac(phi) - th) * 5);
+            em *= 0.5 + Random(seed + 1);
+
+            half4 c = tex2D(_MainTex, IN.uv_MainTex) * _Color;
+            bool backface = IN.vface < 0;
+            o.Albedo = backface ? _Color2.rgb : c.rgb;
+            o.Metallic = backface ? _Metallic2 : _Metallic;
+            o.Smoothness = backface ? _Glossiness2 : _Glossiness;
+            o.Emission = (backface ? 0 : _EmissionColor) + em * _EffectorColor;
+        }
+
+        ENDCG
+    }
+    FallBack "Diffuse"
+}
